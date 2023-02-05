56b7b2b1ce487a38720fa0b72e7917cc15c3b60c
Kusal Liyanage <117502162+kusalliyanage@users.noreply.github.com>
2023-02-05 21:08:51 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,19 @@
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+
+<hr color="#e6e6ff", size="10px">
+      <div class="f">
+        
+        <h5>Developed by:- Kusal G Liyanage. 519218830</h5>
+        <h5>Copyrights &copy; Allrights resereved</h5>
+      </div>
+
+</body>
+</html>
